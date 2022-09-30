1d3e1786f4ecd48c9d8984a1d719f30fe05f26d3
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-09-30 22:54:36 +0000

    Foundation

--- src/js/index.js
+++ src/js/index.js
@@ -10,3 +10,22 @@ import Home from "./component/home.jsx";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
+
+const [selectColor, setSelectedColor] = useState ("red");
+    //first step we need
+
+
+    return (
+        <div className ="text-center">
+            <div clasName ="pole"></div>
+            <div className ="traffic-light">
+            <div onClick ="setColor" className=""></div>
+            <div onClick ="setColor" className=""></div>
+            <div onClick ="setColor" className=""></div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Home;
